dfd1a148ec899f0f84e79b526bcda6485c69db21
Marcilio Junior <marciliojrs@gmail.com>
2017-10-30 12:00:01 -0200

    Create CoordinatorPath protocol and add helper store properties

--- Classes/Coordinator.swift
+++ Classes/Coordinator.swift
@@ -1,9 +1,34 @@
 import UIKit
 
+public protocol CoordinatorPath { }
+
 public protocol Coordinator: class {
     var window: UIWindow { get }
     var childs: [Coordinator] { get }
 
     init(window: UIWindow)
     func start()
+    func route(to path: CoordinatorPath)
+    func finish()
+}
+
+public extension Coordinator {
+    var currentViewController: UIViewController? {
+        guard let rootViewController = window.rootViewController else { return nil }
+
+        return currentViewController(root: rootViewController)
+    }
+
+    private func currentViewController(root: UIViewController) -> UIViewController? {
+        guard let presented = root.presentedViewController else { return root }
+
+        if let tabBarController = presented as? UITabBarController, let selected = tabBarController.selectedViewController {
+            return currentViewController(root: selected)
+        }
+        else if let navigationController = presented as? UINavigationController, let top = navigationController.topViewController {
+            return currentViewController(root: top)
+        }
+
+        return currentViewController(root: presented)
+    }
 }
